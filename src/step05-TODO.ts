9533b6be2014d68ff7791f9c5e6709215e0e471f
Baurain Olivier <baurain.o@gmail.com>
2022-07-12 09:53:47 +0200

    update todos

--- src/step05-TODO.ts
+++ src/step05-TODO.ts
@@ -22,14 +22,25 @@ async function getProgramAccounts() {
   // TODO: Decode tokenAccounts
 
   // TODO: Get all mints accounts from you token accounts
+  // (use connection.getMultipleAccountsInfo)
+  // https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getMultipleAccountsInfo
 
-  // TODO: Decode mints accounts
-
-  // TODO: Get mints decimals
+  // TODO: Decode mints accounts to get decimals
 
   // TODO: Divide token accounts amounts by 10 ** mintDecimal
 
   // TODO: Display all your token amounts !
+  // EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v: 0.075
+  // Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB: 1.552343
+  // orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE: 2.369503
+  // ...
+
+  // BONUS: convert mints to symbol
+  // https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json
+  // USDC: 0.075
+  // USDT: 1.552343
+  // ORCA: 2.369503
+  // ...
 }
 
 getProgramAccounts()
